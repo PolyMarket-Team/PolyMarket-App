e8dcd68c125f4534663c34ab3bd2f69ffc32896f
Shin Woo <sinwoo1225@gmail.com>
2022-04-08 17:35:02 +0900

    main dart cupertino app 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(PolyMarketApp());
+}
+
+class PolyMarketApp extends StatelessWidget {
+  const PolyMarketApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+        theme: CupertinoThemeData(brightness: Brightness.light),
+        home: CupertinoPageScaffold(
+            navigationBar: CupertinoNavigationBar(
+              middle: Text(
+                "PolyMarket",
+              ),
+            ),
+            child: Center(child: Text("PolyMarket"))));
+  }
+}
