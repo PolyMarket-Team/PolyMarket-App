064aff2d2a362637f320633542375dd100d12082
Shin Woo <sinwoo1225@gmail.com>
2022-04-08 18:35:27 +0900

    splash 화면 추가
    - 스플래시 애니메이션 및 화면 적용
    
    TODO 
    - 아래에 스피닝 아이콘 추가
     -> 현재 사용하는 'animation splash screen' 에서 splash icon외에 위젯을 커스텀할 수 없는 구조여서 라이브 수정해서 사용해야함

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   runApp(PolyMarketApp());
@@ -11,14 +13,44 @@ class PolyMarketApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return CupertinoApp(
         theme: CupertinoThemeData(brightness: Brightness.light),
-        home: CupertinoPageScaffold(
-            navigationBar: CupertinoNavigationBar(
-              middle: Text(
-                "PolyMarket",
-              ),
-            ),
-            child: Center(child: Text("PolyMarket"))));
+        home: AnimatedSplashScreen.withScreenFunction(
+          splash: Image.asset(
+            'assets/images/splash_logo.png',
+          ),
+          screenFunction: () async {
+            // TODO 아래 코드 삭제후 초기 데이터를 fetch 하는 코드 추가
+            Future future = Future.delayed(
+                const Duration(seconds: 1), () => print('async task finish'));
+            await future;
+            return PolyMarketMainPage();
+          },
+          splashTransition: SplashTransition.fadeTransition,
+          pageTransitionType: PageTransitionType.fade,
+          duration: 0,
+          animationDuration: Duration(milliseconds: 1100),
+          splashIconSize: 180,
+        ));
+  }
+}
+
+class PolyMarketMainPage extends StatefulWidget {
+  const PolyMarketMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<PolyMarketMainPage> createState() => _PolyMarketMainPageState();
+}
+
+class _PolyMarketMainPageState extends State<PolyMarketMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text(
+            "PolyMarket",
+          ),
+        ),
+        child: Center(child: Text("PolyMarket")));
   }
 }
